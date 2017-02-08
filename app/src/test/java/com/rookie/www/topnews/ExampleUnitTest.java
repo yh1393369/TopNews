be656463b103a0190c837e88d53dc63fbbd5a9d3
Rookie <170018701@qq.com>
2017-02-08 21:22:28 +0800

    Initial commit

---
+++ app/src/test/java/com/rookie/www/topnews/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.rookie.www.topnews;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
