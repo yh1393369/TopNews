0c6c10d6b319ede3663ff3f7a624eae5f194b2af
Rookie <170018701@qq.com>
2017-02-09 20:55:55 +0800

    Home

--- app/src/main/java/com/rookie/www/topnews/activity/MainActivity.java
+++ app/src/main/java/com/rookie/www/topnews/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.rookie.www.topnews.R;
 import com.rookie.www.topnews.adapter.NewsListAdapter;
@@ -11,6 +12,7 @@ import com.rookie.www.topnews.entity.News;
 import com.rookie.www.topnews.listener.HttpCallbackListener;
 import com.rookie.www.topnews.parse.NewsParse;
 import com.rookie.www.topnews.utils.HttpUtils;
+import com.rookie.www.topnews.utils.ProgressDialogUtil;
 
 import java.util.ArrayList;
 
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lvNews = (ListView) findViewById(R.id.lvMain);
-        //ProgressDialogUtil.showProgressDialog();
+        ProgressDialogUtil.showProgressDialog(MainActivity.this);
         HttpUtils.sendHttpRequest("http://v.juhe.cn/toutiao/index?type=&key=0e2e3d9e573eed9381e672a2ff752cca", new HttpCallbackListener() {
             @Override
             public void onFinish(final String respones) {
@@ -34,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
                         ArrayList<News> newses = NewsParse.getNews(respones);
                         NewsListAdapter adapter = new NewsListAdapter(newses, R.layout.listview_item_news, lvNews);
                         lvNews.setAdapter(adapter);
+                        ProgressDialogUtil.closeProgressDialog();
                     }
                 });
 
@@ -41,7 +44,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onError(Exception e) {
-
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ProgressDialogUtil.closeProgressDialog();
+                        Toast.makeText(MainActivity.this, "加载失败，请稍后重试", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/rookie/www/topnews/utils/ProgressDialogUtil.java
+++ app/src/main/java/com/rookie/www/topnews/utils/ProgressDialogUtil.java
@@ -1,6 +1,7 @@
 package com.rookie.www.topnews.utils;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 
 import com.rookie.www.topnews.MyApplication;
 
@@ -12,9 +13,9 @@ public class ProgressDialogUtil {
 
     private static ProgressDialog progressDialog;
 
-    public static void showProgressDialog(){
+    public static void showProgressDialog(Context context){
         if(progressDialog == null){
-            progressDialog = new ProgressDialog(MyApplication.getContext());
+            progressDialog = new ProgressDialog(context);
             progressDialog.setMessage("正在加载...");
             progressDialog.setCanceledOnTouchOutside(false);
         }
