9057348b48a00ba223882b2e561d4233129b7396
Rookie <170018701@qq.com>
2017-02-15 11:12:36 +0800

    company

---
+++ app/src/main/java/com/rookie/www/topnews/Receiver/BootCompleteReceiver.java
@@ -0,0 +1,18 @@
+package com.rookie.www.topnews.Receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Hi on 2017/2/15.
+ */
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        /*intent = new Intent(context, AdsActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);*/
+    }
+}
